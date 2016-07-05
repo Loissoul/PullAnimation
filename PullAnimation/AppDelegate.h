f91de0896861d7a5a220b427df2be4c48800c36e
Loissoul <937542864@qq.com>
2016-07-05 23:18:08 +0800

    Initial Commit

---
+++ PullAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PullAnimation
+//
+//  Created by Lois_pan on 16/7/5.
+//  Copyright © 2016年 Lois_pan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
